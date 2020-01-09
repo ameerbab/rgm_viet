1c6db015fc99c3cd320d1e0b66fd142d573dd045
Vincent Nguyen <vinhbk2000@gmail.com>
2020-01-09 21:54:34 +0700

    Checkin Settings - validate Get Time From

--- rgm_vn/rgm_vn/doctype/checkin_settings/checkin_settings.py
+++ rgm_vn/rgm_vn/doctype/checkin_settings/checkin_settings.py
@@ -3,8 +3,17 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class CheckinSettings(Document):
-	pass
+	def validate(self):
+		if self.lunch_out_from <= self.morning_duty_in_to:
+			frappe.throw(_(""" Difference between "Get Time from" of "LUNCH CHECK OUT" and "Get Time to" "MORNING DUTY IN" should not be greater than "0:01" """))
+		
+		if self.lunch_in_from <= self.lunch_out_to:
+			frappe.throw(_(""" Difference between "Get Time from" of "LUNCH CHECK IN" and "Get Time to" "LUNCH CHECK OUT" should not be greater than "0:01" """))
+		
+		if self.evening_duty_out_from <= self.lunch_in_to:
+			frappe.throw(_(""" Difference between "Get Time from" of "EVENING DUTY CHECK OUT" and "Get Time to" "LUNCH CHECK IN" should not be greater than "0:01" """))
